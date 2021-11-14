187ae4349929cacb6360f96bada2c11a75ecd3a0
khalled alkarmi <khalled_95@hotmail.com>
2021-11-14 22:17:35 +0200

    add login screen and signup screen without code

--- app/src/main/java/com/wise/studentdelivery/MainActivity.kt
+++ app/src/main/java/com/wise/studentdelivery/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.wise.studentdelivery
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var signupButton:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        signupButton = findViewById(R.id.signup_button)
+
+        signupButton.setOnClickListener(View.OnClickListener {
+                v ->
+            val intent = Intent(this,Singup::class.java )
+            startActivity(intent)
+
+        })
     }
 }
---
+++ app/src/main/java/com/wise/studentdelivery/Singup.kt
@@ -0,0 +1,11 @@
+package com.wise.studentdelivery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Singup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_singup)
+    }
+}
