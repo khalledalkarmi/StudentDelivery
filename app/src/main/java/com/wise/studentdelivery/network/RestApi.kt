54cbea5d07c52311d86f75804a02d292a53fe6cc
khalled alkarmi <khalled_95@hotmail.com>
2021-11-20 02:26:23 +0200

    add getAllUsers

--- app/src/main/java/com/wise/studentdelivery/network/RestApi.kt
+++ app/src/main/java/com/wise/studentdelivery/network/RestApi.kt
@@ -1,13 +1,15 @@
 package com.wise.studentdelivery.network
 
 import com.wise.studentdelivery.model.User
+import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.*
 
 
 interface RestApi {
     @POST("api/v1/users/add")
     fun addUser(@Body user:User):Call<User>
+
+    @GET("api/v1/users")
+     fun getAllUsers():Call<List<User>>
 }
--- app/src/main/java/com/wise/studentdelivery/network/RestApiServer.kt
+++ app/src/main/java/com/wise/studentdelivery/network/RestApiServer.kt
@@ -4,10 +4,9 @@ import com.wise.studentdelivery.model.User
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
 
 class RestApiServer {
-    fun addUser(userData: User, onResult: (User?) -> Unit){
+    fun addUser(userData: User, onResult: (User?) -> Unit) {
         val retrofit = ServiceBuilder.buildService(RestApi::class.java)
         retrofit.addUser(userData).enqueue(
             object : Callback<User> {
@@ -15,7 +14,8 @@ class RestApiServer {
                     onResult(null)
                     println(t)
                 }
-                override fun onResponse( call: Call<User>, response: Response<User>) {
+
+                override fun onResponse(call: Call<User>, response: Response<User>) {
                     val addedUser = response.body()
                     println(response)
                     onResult(addedUser)
@@ -23,4 +23,23 @@ class RestApiServer {
             }
         )
     }
+
+      fun getAllUsers(onResult: (List<User>?) -> Unit) {
+        val retrofit= ServiceBuilder.buildService(RestApi::class.java)
+        retrofit.getAllUsers().enqueue(
+            object : Callback<List<User>>{
+                override fun onFailure(call: Call<List<User>>, t: Throwable) {
+                    onResult(null)
+                    println("$t failed in get user by email")
+                }
+
+                override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+                    val  findUserByEmail = response.body()
+                    println("${response.body()} get user by email successful")
+                    onResult(findUserByEmail)
+                }
+            }
+        )
+    }
+
 }
