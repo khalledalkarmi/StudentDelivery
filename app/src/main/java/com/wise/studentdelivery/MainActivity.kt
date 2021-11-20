ec0ed5c60ed470ad3bf14433c7e860d5ad637cd7
khalled alkarmi <khalled_95@hotmail.com>
2021-11-20 02:26:04 +0200

    try to implement login

--- app/src/main/java/com/wise/studentdelivery/MainActivity.kt
+++ app/src/main/java/com/wise/studentdelivery/MainActivity.kt
@@ -4,21 +4,36 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import com.wise.studentdelivery.network.RestApiServer
 
 class MainActivity : AppCompatActivity() {
-    lateinit var signupButton:TextView
+    lateinit var signupButton: TextView
+    lateinit var email: EditText
+    lateinit var password: EditText
+    lateinit var loginButton: Button
+    lateinit var apiServer: RestApiServer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        email = findViewById(R.id.email_text)
+        password = findViewById(R.id.password_text)
+        loginButton = findViewById(R.id.login_button)
         signupButton = findViewById(R.id.signup_button)
-
-        signupButton.setOnClickListener(View.OnClickListener {
-                v ->
-            val intent = Intent(this,Signup::class.java )
+        apiServer = RestApiServer()
+        signupButton.setOnClickListener(View.OnClickListener { v ->
+            val intent = Intent(this, Signup::class.java)
             startActivity(intent)
-
         })
+
+        loginButton.setOnClickListener { v ->
+
+        TODO("not implemented yet :)")
+        }
     }
+
+
 }
