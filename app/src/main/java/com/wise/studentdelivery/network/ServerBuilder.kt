2ca9c5ccde1186e9207ea259f2dde90191b0b63d
khalled alkarmi <khalled_95@hotmail.com>
2021-11-21 12:03:36 +0200

    add gsonBuilder to make retrofit accept any respond body

--- app/src/main/java/com/wise/studentdelivery/network/ServerBuilder.kt
+++ app/src/main/java/com/wise/studentdelivery/network/ServerBuilder.kt
@@ -1,19 +1,28 @@
 package com.wise.studentdelivery.network
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceBuilder {
     private val client = OkHttpClient.Builder().build()
+    // to make retrofit to accept all body not only json
+    private val gsonBuilder = GsonBuilder().setLenient()
+    private val gson: Gson = gsonBuilder.create()
+
+
+// and in you adapter set this instance
+
 
     private val retrofit = Retrofit.Builder()
         .baseUrl("http://10.0.2.2:8080/") // change this IP for testing by your actual machine IP
-        .addConverterFactory(GsonConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create(gson))
         .client(client)
         .build()
 
-    fun<T> buildService(service: Class<T>): T{
+    fun <T> buildService(service: Class<T>): T {
         return retrofit.create(service)
     }
 }
