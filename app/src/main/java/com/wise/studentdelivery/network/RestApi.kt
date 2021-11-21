4dfaf7af3ae4e70eddd56f90f7baed7fcd3587cd
khalled alkarmi <khalled_95@hotmail.com>
2021-11-21 12:05:29 +0200

    add CheckIfUserExistByEmail function

--- app/src/main/java/com/wise/studentdelivery/network/RestApi.kt
+++ app/src/main/java/com/wise/studentdelivery/network/RestApi.kt
@@ -8,8 +8,11 @@ import retrofit2.http.*
 
 interface RestApi {
     @POST("api/v1/users/add")
-    fun addUser(@Body user:User):Call<User>
+    fun addUser(@Body user: User): Call<User>
 
     @GET("api/v1/users")
-     fun getAllUsers():Call<List<User>>
+    fun getAllUsers(): Call<List<User>>
+
+    @GET("api/v1/users/email/{email}")
+    fun checkIfUserExist(@Path("email") email: String): Call<String>
 }
--- app/src/main/java/com/wise/studentdelivery/network/RestApiServer.kt
+++ app/src/main/java/com/wise/studentdelivery/network/RestApiServer.kt
@@ -24,22 +24,42 @@ class RestApiServer {
         )
     }
 
-      fun getAllUsers(onResult: (List<User>?) -> Unit) {
-        val retrofit= ServiceBuilder.buildService(RestApi::class.java)
+    fun getAllUsers(onResult: (List<User>?) -> Unit) {
+        val retrofit = ServiceBuilder.buildService(RestApi::class.java)
         retrofit.getAllUsers().enqueue(
-            object : Callback<List<User>>{
+            object : Callback<List<User>> {
                 override fun onFailure(call: Call<List<User>>, t: Throwable) {
                     onResult(null)
-                    println("$t failed in get user by email")
+                    println("$t failed in get all user ")
                 }
 
                 override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
-                    val  findUserByEmail = response.body()
-                    println("${response.body()} get user by email successful")
+                    val findUserByEmail = response.body()
+                    println("${response.body()} get all user successful")
                     onResult(findUserByEmail)
                 }
             }
         )
     }
 
+    fun checkIfUserExistByEmail(email: String, onResult: (String?) -> Unit) {
+        val retrofit = ServiceBuilder.buildService(RestApi::class.java)
+        retrofit.checkIfUserExist(email).enqueue(
+            object : Callback<String> {
+
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    val checkIfUserExistByEmail = response.body()
+                    println("${response.body()} get user by email successful")
+                    onResult(checkIfUserExistByEmail)
+
+                }
+
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    onResult("User not exist")
+                    println("$t failed in get user by email")
+                }
+
+            }
+        )
+    }
 }
