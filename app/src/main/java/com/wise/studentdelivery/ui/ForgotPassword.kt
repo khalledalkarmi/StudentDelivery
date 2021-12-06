7f22a5321883ac93a003f6c9dc116038ccff8b48
khalled alkarmi <khalled_95@hotmail.com>
2021-12-06 19:09:36 +0200

    fix typo

--- app/src/main/java/com/wise/studentdelivery/ui/ForgotPassword.kt
+++ app/src/main/java/com/wise/studentdelivery/ui/ForgotPassword.kt
@@ -5,15 +5,15 @@ import android.os.Bundle
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.wise.studentdelivery.R
-import com.wise.studentdelivery.ui.fragment.ForgetPasswordFragment
+import com.wise.studentdelivery.ui.fragment.ForgotPasswordFragment
 
-class ForgetPassword : AppCompatActivity(R.layout.activity_forget_password) {
+class ForgotPassword : AppCompatActivity(R.layout.activity_forget_password) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (savedInstanceState == null){
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
-                add<ForgetPasswordFragment>(R.id.fragmentContainerView)
+                add<ForgotPasswordFragment>(R.id.fragmentContainerView)
             }
         }
     }
