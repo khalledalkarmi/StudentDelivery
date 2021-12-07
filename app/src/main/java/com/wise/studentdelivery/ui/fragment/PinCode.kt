76eb2d959713fd3b8b9a2744eb1ed45dbe103ab8
khalled alkarmi <khalled_95@hotmail.com>
2021-12-07 17:14:44 +0200

    ad PinCode.kt and fragment_pin_code.xml

---
+++ app/src/main/java/com/wise/studentdelivery/ui/fragment/PinCode.kt
@@ -0,0 +1,21 @@
+package com.wise.studentdelivery.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.wise.studentdelivery.R
+
+class PinCode : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pin_code, container, false)
+    }
+}
