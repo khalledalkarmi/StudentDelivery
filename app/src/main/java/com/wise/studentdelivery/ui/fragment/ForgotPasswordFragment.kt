058e225568c11942da0cd22e969dad898d32379b
khalled alkarmi <khalled_95@hotmail.com>
2021-12-07 17:14:21 +0200

    add piValdir

--- app/src/main/java/com/wise/studentdelivery/ui/fragment/ForgotPasswordFragment.kt
+++ app/src/main/java/com/wise/studentdelivery/ui/fragment/ForgotPasswordFragment.kt
@@ -1,13 +1,12 @@
 package com.wise.studentdelivery.ui.fragment
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import androidx.fragment.app.*
 import com.wise.studentdelivery.R
 import com.wise.studentdelivery.network.RestApiServer
 import com.wise.studentdelivery.utilities.Validator
@@ -18,10 +17,11 @@ import com.wise.studentdelivery.utilities.Validator
  * create an instance of this fragment.
  */
 class ForgotPasswordFragment : Fragment() {
-    lateinit var forgetPasswordButton:Button
-    lateinit var emailAddress:EditText
+    lateinit var forgetPasswordButton: Button
+    lateinit var emailAddress: EditText
     private val validator = Validator()
     lateinit var apiServer: RestApiServer
+    var pinCode: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,21 +40,36 @@ class ForgotPasswordFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        forgetPasswordButton = view.findViewById(R.id.forget_password_fragment_button)
-        emailAddress = view.findViewById(R.id.email_address_forget_fragment)
 
+        forgetPasswordButton = view.findViewById(R.id.pin_code_fragment_button)
+        emailAddress = view.findViewById(R.id.pin_code_text)
+        val pinCode = PinCode()
         forgetPasswordButton.setOnClickListener {
-            if (emailValdir()){
-                apiServer.getPIN(emailAddress.text.toString()){
+            if (emailValdir()) {
+                apiServer.getPIN(emailAddress.text.toString()) {
                     println(it)
+                    if (pinValdir(it.toString())
+                    ) {
+                        parentFragmentManager.commit {
+                            replace(R.id.fragmentContainerView, pinCode)
+                        }
+                    }
                 }
             }
-
         }
+
     }
 
-    private fun emailValdir():Boolean {
+
+    private fun emailValdir(): Boolean {
         return validator.isValidEmail(emailAddress)
     }
 
+    private fun pinValdir(pin: String): Boolean {
+        if (pin.toInt() > 0) {
+            pinCode = pin.toInt()
+            return true
+        }
+        return false
+    }
 }
