939851e1e700d22e7ecd2f5ffb0126c5cb6074cb
khalled alkarmi <khalled_95@hotmail.com>
2021-12-12 11:16:42 +0200

    add textview for display error message when email or password is mot correct

--- app/src/main/java/com/wise/studentdelivery/MainActivity.kt
+++ app/src/main/java/com/wise/studentdelivery/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.wise.studentdelivery
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.core.view.isVisible
 import com.wise.studentdelivery.network.RestApiServer
 import com.wise.studentdelivery.ui.ForgotPassword
 import com.wise.studentdelivery.ui.Signup
@@ -22,17 +24,21 @@ class MainActivity : AppCompatActivity() {
     private lateinit var email: EditText
     private lateinit var password: EditText
     private lateinit var loginButton: Button
+    lateinit var errorText: TextView
     private lateinit var apiServer: RestApiServer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        errorText = findViewById(R.id.error_text)
         email = findViewById(R.id.email_text)
         password = findViewById(R.id.password_text)
         loginButton = findViewById(R.id.login_button)
         signupButton = findViewById(R.id.signup_button)
         forgetPasswordButton = findViewById(R.id.forget_password_text)
         apiServer = RestApiServer()
+
+        errorText.isVisible = false
         signupButton.setOnClickListener {
             val intent = Intent(this, Signup::class.java)
             startActivity(intent)
@@ -40,13 +46,17 @@ class MainActivity : AppCompatActivity() {
 
         loginButton.setOnClickListener {
             val email = email.text.toString()
-            var userVerified= false
             if (checkIfUserExist(email).toString().isNotEmpty()) {
                 apiServer.getUserPassword(email) {
-                    println("$it password for $email")
+                    println("$it password from server for $email")
                     if (it.toString() == password.text.toString()) {
                         //TODO: create request activity and goto it
-                        println("${it.toString()} $email")
+                        println("password ${it.toString()} is match the $email password")
+                        errorText.isVisible = false
+                    }else {
+                        errorText.isVisible = true
+                        errorText.setText(R.string.email_error)
+                        errorText.setTextColor(Color.RED)
                     }
                 }
             }
@@ -60,6 +70,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkIfUserExist(email: String) {
-        return apiServer.checkIfUserExistByEmail(email = email) {}
+        return apiServer.checkIfUserExistByEmail(email = email) {
+            if (it.isNullOrEmpty()) {
+                errorText.setText(R.string.email_error)
+                errorText.setTextColor(Color.RED)
+            }
+        }
     }
 }
