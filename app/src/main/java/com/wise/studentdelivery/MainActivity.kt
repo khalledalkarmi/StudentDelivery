dc38429736a3c1dce01553e3e0d6445900c52be8
khalled alkarmi <khalled_95@hotmail.com>
2021-12-15 10:19:01 +0200

    change todo

--- app/src/main/java/com/wise/studentdelivery/MainActivity.kt
+++ app/src/main/java/com/wise/studentdelivery/MainActivity.kt
@@ -1,21 +1,21 @@
 package com.wise.studentdelivery
 
 import android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.core.view.isVisible
 import com.wise.studentdelivery.network.RestApiServer
 import com.wise.studentdelivery.ui.ForgotPassword
 import com.wise.studentdelivery.ui.Signup
 
 /*
 TODO: make request to rewrite password
-TODO: check if internet connected by add fragment on mainActivity
+TODO: check if internet connected
+TODO: check if email not exist and show error in textView
+TODO: get password from database and compare it with entered password
+
  */
 class MainActivity : AppCompatActivity() {
     private lateinit var signupButton: TextView
@@ -23,23 +23,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var email: EditText
     private lateinit var password: EditText
     private lateinit var loginButton: Button
-    lateinit var errorText: TextView
     private lateinit var apiServer: RestApiServer
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        errorText = findViewById(R.id.error_text)
         email = findViewById(R.id.email_text)
         password = findViewById(R.id.password_text)
         loginButton = findViewById(R.id.login_button)
         signupButton = findViewById(R.id.signup_button)
         forgetPasswordButton = findViewById(R.id.forget_password_text)
         apiServer = RestApiServer()
-
-        errorText.isVisible = false
-        signupButton.setOnClickListener {
+        signupButton.setOnClickListener{
             val intent = Intent(this, Signup::class.java)
             startActivity(intent)
         }
@@ -48,19 +43,10 @@ class MainActivity : AppCompatActivity() {
             val email = email.text.toString()
             if (checkIfUserExist(email).toString().isNotEmpty()) {
                 apiServer.getUserPassword(email) {
-                    println("$it password from server for $email")
-                    if (it.toString() == password.text.toString()) {
-                        //TODO: create request activity and goto it
-                        println("password ${it.toString()} is match the $email password")
-                        errorText.isVisible = false
-                    } else {
-                        errorText.isVisible = true
-                        errorText.setText(R.string.email_error)
-                        errorText.setTextColor(Color.RED)
-                    }
+                    println("$it password for $email")
+                    //TODO: create request activity and goto it
                 }
             }
-
         }
 
         forgetPasswordButton.setOnClickListener {
@@ -70,11 +56,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkIfUserExist(email: String) {
-        return apiServer.checkIfUserExistByEmail(email = email) {
-            if (it.isNullOrEmpty()) {
-                errorText.setText(R.string.email_error)
-                errorText.setTextColor(Color.RED)
-            }
-        }
+        return apiServer.checkIfUserExistByEmail(email = email) {}
     }
 }
