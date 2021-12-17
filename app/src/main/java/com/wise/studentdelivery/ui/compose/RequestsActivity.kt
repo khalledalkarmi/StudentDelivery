6b859ef77da254ad7e469bf23faf39f05dc66052
khalled alkarmi <khalled_95@hotmail.com>
2021-12-17 23:02:54 +0200

    add jetpack compose to project

---
+++ app/src/main/java/com/wise/studentdelivery/ui/compose/RequestsActivity.kt
@@ -0,0 +1,38 @@
+package com.wise.studentdelivery.ui.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.wise.studentdelivery.ui.compose.ui.theme.StudentDeliveryTheme
+
+class RequestsActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StudentDeliveryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    StudentDeliveryTheme {
+        Greeting("Android")
+    }
+}
