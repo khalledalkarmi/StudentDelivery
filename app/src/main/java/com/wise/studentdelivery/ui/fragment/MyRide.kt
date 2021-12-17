4e8024877b785f6d1b764001f69f83ffa5d81a4e
khalled alkarmi <khalled_95@hotmail.com>
2021-12-17 22:21:30 +0200

    override onCreate onCreateView onCreatedView

--- app/src/main/java/com/wise/studentdelivery/ui/fragment/MyRide.kt
+++ app/src/main/java/com/wise/studentdelivery/ui/fragment/MyRide.kt
@@ -1,6 +1,24 @@
 package com.wise.studentdelivery.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.*
 
 class MyRide : Fragment(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
