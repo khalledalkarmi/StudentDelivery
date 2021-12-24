e3618a2226c4cd31523622115cbb16b2d8f32009
khalled alkarmi <khalled_95@hotmail.com>
2021-12-25 00:19:51 +0200

    add MainFunActivity.kt to as container for nav fragment

---
+++ app/src/main/java/com/wise/studentdelivery/ui/MainFunActivity.kt
@@ -0,0 +1,12 @@
+package com.wise.studentdelivery.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wise.studentdelivery.R
+
+class MainFunActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_fun)
+    }
+}
