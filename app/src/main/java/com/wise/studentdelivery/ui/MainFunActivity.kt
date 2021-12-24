f2847fa606b4bdac10ff4703aa0ea6fc09356c71
khalled alkarmi <khalled_95@hotmail.com>
2021-12-25 00:21:48 +0200

    add fragment support manager

--- app/src/main/java/com/wise/studentdelivery/ui/MainFunActivity.kt
+++ app/src/main/java/com/wise/studentdelivery/ui/MainFunActivity.kt
@@ -2,11 +2,20 @@ package com.wise.studentdelivery.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.wise.studentdelivery.R
+import com.wise.studentdelivery.ui.fragment.BottomNavigationFragment
+import com.wise.studentdelivery.ui.fragment.ForgotPasswordFragment
 
 class MainFunActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_fun)
+        if (savedInstanceState == null){
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<BottomNavigationFragment>(R.id.fragmentContainerViewMainFun)
+            }
+        }
     }
 }
