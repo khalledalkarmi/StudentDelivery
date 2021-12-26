ca5c0ae1377940123002876cf8f6797f10fa8654
khalled alkarmi <khalled_95@hotmail.com>
2021-12-27 01:53:56 +0200

    implement nav to ProfileFragment implement nav to setNewPassword Fragment

--- app/src/main/java/com/wise/studentdelivery/ui/fragment/MainProfileFragment.kt
+++ app/src/main/java/com/wise/studentdelivery/ui/fragment/MainProfileFragment.kt
@@ -8,9 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import com.wise.studentdelivery.R
 import com.wise.studentdelivery.network.RestApiServer
+import kotlin.math.log
+
 //TODO: implement button navigation
 class MainProfileFragment : Fragment() {
 
@@ -20,16 +24,17 @@ class MainProfileFragment : Fragment() {
     private lateinit var savedRequestButton: Button
     private lateinit var accountSittingButton:Button
     private lateinit var reportProblemButton:Button
+    private lateinit var changePasswordButton: Button
+    private lateinit var profileFragment: ProfileFragment
+    private lateinit var setNewPassword: SetNewPassword
+    private lateinit var logOutButton: Button
+    private lateinit var email:String
     private lateinit var apiServer: RestApiServer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         apiServer=RestApiServer()
-
-
-
-
-
-
+        profileFragment = ProfileFragment()
+        setNewPassword = SetNewPassword()
     }
 
     override fun onCreateView(
@@ -37,6 +42,7 @@ class MainProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        email = requireArguments().getString(email).toString()
         return inflater.inflate(R.layout.fragment_main_profile, container, false)
     }
 
@@ -46,11 +52,27 @@ class MainProfileFragment : Fragment() {
         myRequestButton = view.findViewById(R.id.my_requset)
         savedRequestButton = view.findViewById(R.id.saved_request)
         accountSittingButton = view.findViewById(R.id.account_information)
-        reportProblemButton = view.findViewById(R.id.change_password)
+        changePasswordButton = view.findViewById(R.id.change_password)
+        logOutButton = view.findViewById(R.id.log_out)
+        reportProblemButton = view.findViewById(R.id.report_a_problem)
 
+        accountSittingButton.setOnClickListener {
+            parentFragmentManager.commit {
+                val bundle= bundleOf("email" to email)
+                profileFragment.arguments = bundle
+                replace(R.id.fragmentContainerViewMainFun,profileFragment)
+            }
+        }
 
+        changePasswordButton.setOnClickListener {
+            parentFragmentManager.commit {
+                val bundle= bundleOf("email" to email)
+                setNewPassword.arguments = bundle
+                replace(R.id.fragmentContainerViewMainFun,setNewPassword)
+            }
+        }
         //TODO: pass email from any activity
-        apiServer.getImage("khalled_95@hotmail.com"){
+        apiServer.getImage(email = email){
             println(it.toString())
             if (it != null) {
                 val imageBytes = Base64.decode(it.data, Base64.DEFAULT)
