6c746d819dc2cc6200eb3971978e6db6acbf5f0a
khalled alkarmi <khalled_95@hotmail.com>
2021-12-27 00:56:11 +0200

    pass email to RequestsViewFragment.kt

--- app/src/main/java/com/wise/studentdelivery/ui/fragment/BottomNavigationFragment.kt
+++ app/src/main/java/com/wise/studentdelivery/ui/fragment/BottomNavigationFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -12,6 +13,7 @@ import com.wise.studentdelivery.ui.compose.RequestsViewFragment
 
 //TODO: use navHost for navigation
 class BottomNavigationFragment : Fragment() {
+    private lateinit var email:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -21,6 +23,8 @@ class BottomNavigationFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        email = requireArguments().getString("email").toString()
+
         return inflater.inflate(R.layout.bottom_navigation_bar, container, false)
 
     }
@@ -28,6 +32,8 @@ class BottomNavigationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val requestsViewFragment = RequestsViewFragment()
+        val bundle = bundleOf("email" to email)
+        requestsViewFragment.arguments= bundle
         parentFragmentManager.commit {
             setReorderingAllowed(true)
             replace(R.id.fragmentContainerViewMain,requestsViewFragment)
