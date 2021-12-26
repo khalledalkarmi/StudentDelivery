5727d1729db8669c551ed3c932aaf2e24d7ccb48
khalled alkarmi <khalled_95@hotmail.com>
2021-12-27 00:55:32 +0200

    get email from MainActivity.kt and it to BottomNavigationFragment.kt

--- app/src/main/java/com/wise/studentdelivery/ui/MainFunActivity.kt
+++ app/src/main/java/com/wise/studentdelivery/ui/MainFunActivity.kt
@@ -2,6 +2,7 @@ package com.wise.studentdelivery.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.os.bundleOf
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.wise.studentdelivery.R
@@ -10,12 +11,15 @@ import com.wise.studentdelivery.ui.fragment.BottomNavigationFragment
 import com.wise.studentdelivery.ui.fragment.ForgotPasswordFragment
 
 class MainFunActivity : AppCompatActivity(R.layout.activity_main_fun) {
+    private lateinit var email: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (savedInstanceState == null){
+        if (savedInstanceState == null) {
+            email = intent.getStringExtra("email").toString()
+            val bundle = bundleOf("email" to email)
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
-                add<BottomNavigationFragment>(R.id.fragmentContainerViewMainFun)
+                add<BottomNavigationFragment>(R.id.fragmentContainerViewMainFun, args = bundle)
             }
         }
     }
