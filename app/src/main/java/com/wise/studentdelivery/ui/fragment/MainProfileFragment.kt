512f10b89279216de2756db4024f48e0bf8bf5b2
khalled alkarmi <khalled_95@hotmail.com>
2022-01-07 01:03:48 +0200

    implement add/edit image and upload to server

--- app/src/main/java/com/wise/studentdelivery/ui/fragment/MainProfileFragment.kt
+++ app/src/main/java/com/wise/studentdelivery/ui/fragment/MainProfileFragment.kt
@@ -21,29 +21,29 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.wise.studentdelivery.R
 import com.wise.studentdelivery.network.RestApiServer
-//TODO: implement upload image to database
-//TODO: implement button navigation
+import com.wise.studentdelivery.utilities.RealPathUtil
+
 class MainProfileFragment : Fragment() {
 
 
-    private lateinit var profileImage :ImageView
+    private lateinit var profileImage: ImageView
     private lateinit var changeImageButton: ImageButton
     private lateinit var nameTextView: TextView
     private lateinit var myRequestButton: Button
-    private lateinit var accountSittingButton:Button
-    private lateinit var reportProblemButton:Button
+    private lateinit var accountSittingButton: Button
+    private lateinit var reportProblemButton: Button
     private lateinit var changePasswordButton: Button
     private lateinit var profileFragment: ProfileFragment
     private lateinit var setNewPassword: SetNewPassword
-    private lateinit var myRide:MyRide
+    private lateinit var myRide: MyRide
     private lateinit var logOutButton: Button
-    private lateinit var email:String
+    private lateinit var email: String
     private lateinit var apiServer: RestApiServer
     private val REQUEST_IMAGE_GALLERY = 132
     private val REQUEST_IMAGE_CAMERA = 142
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        apiServer=RestApiServer()
+        apiServer = RestApiServer()
         profileFragment = ProfileFragment()
         setNewPassword = SetNewPassword()
         myRide = MyRide()
@@ -72,29 +72,29 @@ class MainProfileFragment : Fragment() {
 
         accountSittingButton.setOnClickListener {
             parentFragmentManager.commit {
-                val bundle= bundleOf("email" to email)
+                val bundle = bundleOf("email" to email)
                 addToBackStack("mainProfile")
                 profileFragment.arguments = bundle
-                replace(R.id.fragmentContainerViewMainFun,profileFragment)
+                replace(R.id.fragmentContainerViewMainFun, profileFragment)
             }
         }
 
         changePasswordButton.setOnClickListener {
             parentFragmentManager.commit {
-                val bundle= bundleOf("email" to email)
+                val bundle = bundleOf("email" to email)
                 addToBackStack("changePassword")
                 setNewPassword.arguments = bundle
-                replace(R.id.fragmentContainerViewMainFun,setNewPassword)
+                replace(R.id.fragmentContainerViewMainFun, setNewPassword)
             }
         }
 
         myRequestButton.setOnClickListener {
-                parentFragmentManager.commit {
-                    val bundle = bundleOf("email" to email)
-                    myRide.arguments = bundle
-                    addToBackStack("myRide")
-                    replace(R.id.fragmentContainerViewMainFun,myRide)
-                }
+            parentFragmentManager.commit {
+                val bundle = bundleOf("email" to email)
+                myRide.arguments = bundle
+                addToBackStack("myRide")
+                replace(R.id.fragmentContainerViewMainFun, myRide)
+            }
         }
         apiServer.getUserByEmail(email) { user ->
             if (user != null) {
@@ -102,7 +102,7 @@ class MainProfileFragment : Fragment() {
             }
         }
 
-        apiServer.getImage(email = email){
+        apiServer.getImage(email = email) {
             println(it.toString())
             if (it != null) {
                 val imageBytes = Base64.decode(it.data, Base64.DEFAULT)
@@ -110,12 +110,12 @@ class MainProfileFragment : Fragment() {
                 profileImage.setImageBitmap(decodedImage)
             }
         }
-        changeImageButton.setOnClickListener{
+        changeImageButton.setOnClickListener {
 
             val builder = AlertDialog.Builder(activity!!)
             builder.setTitle("Select Image")
             builder.setMessage("Choose your option:")
-            builder.setPositiveButton("Gallery")  { dialog, which ->
+            builder.setPositiveButton("Gallery") { dialog, which ->
                 dialog.dismiss()
 
                 val intent = Intent(Intent.ACTION_PICK)
@@ -123,25 +123,30 @@ class MainProfileFragment : Fragment() {
                 startActivityForResult(intent, REQUEST_IMAGE_GALLERY)
 
             }
-            builder.setNegativeButton("Camera")  { dialog, which ->
+            builder.setNegativeButton("Camera") { dialog, which ->
                 dialog.dismiss()
 
                 Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-                    takePictureIntent.resolveActivity(activity!!.packageManager)?.also{
-                        val permission = ContextCompat.checkSelfPermission(activity!!, android.Manifest.permission.CAMERA)
+                    takePictureIntent.resolveActivity(activity!!.packageManager)?.also {
+                        val permission = ContextCompat.checkSelfPermission(
+                            activity!!,
+                            android.Manifest.permission.CAMERA
+                        )
 
-                        if(permission != PackageManager.PERMISSION_GRANTED){
-                            ActivityCompat.requestPermissions(activity!!, arrayOf(android.Manifest.permission.CAMERA),1)
-                        }
-
-                        else{
+                        if (permission != PackageManager.PERMISSION_GRANTED) {
+                            ActivityCompat.requestPermissions(
+                                activity!!,
+                                arrayOf(android.Manifest.permission.CAMERA),
+                                1
+                            )
+                        } else {
                             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAMERA)
                         }
                     }
                 }
             }
 
-            val dialog :AlertDialog = builder.create()
+            val dialog: AlertDialog = builder.create()
             dialog.show()
 
         }
@@ -150,13 +155,16 @@ class MainProfileFragment : Fragment() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(requestCode == REQUEST_IMAGE_GALLERY && resultCode == Activity.RESULT_OK && data != null){
+        if (requestCode == REQUEST_IMAGE_GALLERY && resultCode == Activity.RESULT_OK && data != null) {
             profileImage.setImageURI(data.data)
-        }else if(requestCode == REQUEST_IMAGE_CAMERA && resultCode == Activity.RESULT_OK && data != null){
+            val imageRealPath = RealPathUtil.getRealPath(requireContext().applicationContext,data.data!!)
+            apiServer.setImageByEmail(imageRealPath, email){
+
+            }
+        } else if (requestCode == REQUEST_IMAGE_CAMERA && resultCode == Activity.RESULT_OK && data != null) {
             profileImage.setImageBitmap(data.extras?.get("data") as Bitmap)
-        }
-        else{
-            Toast.makeText(activity!!,"Something went wrong", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(activity!!, "Something went wrong", Toast.LENGTH_SHORT).show()
         }
     }
-    }
+}
