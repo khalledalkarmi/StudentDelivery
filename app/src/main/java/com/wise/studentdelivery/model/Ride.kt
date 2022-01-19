56eca27ad20036cfb03b298c6a14dae255677b90
khalled alkarmi <khalled_95@hotmail.com>
2022-01-19 16:02:29 +0200

    add car Model to Ride model

--- app/src/main/java/com/wise/studentdelivery/model/Ride.kt
+++ app/src/main/java/com/wise/studentdelivery/model/Ride.kt
@@ -1,18 +1,19 @@
 package com.wise.studentdelivery.model
 
 data class Ride(
-    val email:String,
-    val firstName:String,
-    val lastName:String,
+    val email: String,
+    val firstName: String,
+    val lastName: String,
     val photo: Photo?,
     val goTime: String,
     val comeBackTime: String,
     val uniName: String?,
     val cityName: String?,
     val neighborhoodName: String?,
-    val emptySeats:String,
-    val price:String,
-    val extraDetails:String,
-    val genderSpecific:String,
-    val isPrivate:String
+    val emptySeats: String,
+    val price: String,
+    val extraDetails: String,
+    val genderSpecific: String,
+    val car: Car,
+    val isPrivate: String
 )
